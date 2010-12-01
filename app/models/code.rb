1110dd383a513d402333b04455711d840fe41866
Harry Brundage <harry@skylightlabs.ca>
2010-12-01 18:11:43 -0500

    Made sure code shortcodes were unique

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,14 +1,22 @@
 class Code
   include MongoMapper::Document         
 
-  key :shortcode, String
-  key :splash_id, ObjectId
+  key :shortcode, String, :required => true, :unique => true
+  key :splash_id, ObjectId, :required => true
   
   belongs_to :splash
   validates_presence_of :shortcode, :splash_id
   validates_format_of :shortcode, :with => /^[a-zA-Z0-9]+$/, :message => "must contain only alphanumeric characters (no punctuation or spaces, please.)"
   validates_length_of :shortcode, :in => 3..20
+  validates_uniqueness_of :shortcode
 
+  def initialize(attrs={})
+    super(attrs)
+    unless self.shortcode.present?
+      o =  [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten 
+      self.shortcode = (0..5).map{ o[rand(o.length)]  }.join
+    end
+  end
   def full_url
     (Rails.env == "development" ? "http://localhost:3000/s/" : "http://citrus.heroku.com/s/") + self.shortcode
   end

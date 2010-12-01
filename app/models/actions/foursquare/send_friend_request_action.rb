507c5a80dcade7fffb0e3116bcd5d57423149c77
Harry Brundage <harry@skylightlabs.ca>
2010-12-01 02:53:31 -0500

    Adding some stuff

---
+++ app/models/actions/foursquare/send_friend_request_action.rb
@@ -0,0 +1,14 @@
+module Actions
+  module Foursquare
+    class SendFriendRequestAction < FoursquareAction
+      key :user_id, String
+      validates_presence_of :user_id      
+      attr_accessible :user_id
+      def self.default_action_text
+        "Add as a foursquare friend"
+      end
+    end
+  end
+end
+
+
